67a4baee8014dc94412c2a0790c782d36f28784d
Alicia <aliciakfinacio@gmail.com>
2024-12-01 20:36:03 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+  {
+    numb: 1,
+    question: "O que é o ES6 em JavaScript?",
+    answer:
+      "C. A versão ECMAScript 2015 que introduziu novos recursos no JavaScript",
+    options: [
+      "A. Uma versão antiga do JavaScript",
+      "B. Uma ferramenta para debugar código JavaScript",
+      "C. A versão ECMAScript 2015 que introduziu novos recursos no JavaScript",
+      "D. Uma biblioteca para manipular o DOM",
+    ],
+  },
+  {
+    numb: 2,
+    question: "O que a tag <br> faz em HTML?",
+    answer: "a) Cria uma quebra de linha",
+    options: [
+      "a) Cria uma quebra de linha",
+      "b) Insere um link",
+      "c) Adiciona um rodapé",
+      "d) Formata texto em negrito",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Qual é a diferença entre classes e IDs no CSS?",
+    answer:
+      "b) IDs são exclusivos para um único elemento, e classes podem ser usadas para múltiplos elementos",
+    options: [
+      "a) IDs são para múltiplos elementos e classes para um único elemento",
+      "b) IDs são exclusivos para um único elemento, e classes podem ser usadas para múltiplos elementos",
+      "c) Classes e IDs são a mesma coisa no CSS",
+      "d) Classes são usadas para estilizar páginas inteiras, e IDs para páginas específicas",
+    ],
+  },
+  {
+    numb: 4,
+    question: "O que significa DOM em JavaScript?",
+    answer:
+      "b) Document Object Model, a representação em árvore da estrutura HTML de uma página",
+    options: [
+      "a) Digital Object Manager",
+      "b) Document Object Model, a representação em árvore da estrutura HTML de uma página",
+      "c) Design of Modules, a estrutura de módulos de uma página",
+      "d) Digital Operating Model, uma técnica de otimização de código",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Qual é a função de um framework JavaScript?",
+    answer:
+      "d) Facilitar o desenvolvimento de aplicações web, como React ou Angular",
+    options: [
+      "a) Facilitar a criação de páginas web estáticas",
+      "b) Fornecer ferramentas para manipulação do DOM",
+      "c) Ajudar a criar layouts responsivos sem a necessidade de CSS",
+      "d) Facilitar o desenvolvimento de aplicações web, como React ou Angular",
+    ],
+  },
+];
